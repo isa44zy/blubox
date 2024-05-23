9444ca42754839f9a3d896e9cfb1e800feab5ab4
isa44zy <162628718+isa44zy@users.noreply.github.com>
2024-05-23 14:18:55 -0300

    blue box

---
+++ src/test/java/com/blueBox/BlueboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
